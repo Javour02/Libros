1c8238cd21f4c91ae64219a0187d8bf9e8139d47
Javour02 <javiergach@unisabana.edu.co>
2022-03-01 13:19:55 -0500

    Navbar y vista especifica de libro

--- libros-app/src/App.js
+++ libros-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import Navbar from './Navbar';
+import VistaEspecifica from './VistaEspecifica';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <VistaEspecifica/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
