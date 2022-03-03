9afd1331596d9f55a30bd5707651a1cce6808753
Javour02 <javiergach@unisabana.edu.co>
2022-03-02 23:00:04 -0500

    vistaEspecifica y ventaLibros prototipo

--- libros-app/src/App.js
+++ libros-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import VistaEspecifica from './VistaEspecifica';
+import VentaLibros from './VentaLibros';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <VistaEspecifica/>
+      <VentaLibros/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
